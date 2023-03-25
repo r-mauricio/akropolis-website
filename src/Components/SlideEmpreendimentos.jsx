20940eebd8291e34253c092e199220150c630a1e
Rafael Mauricio <rafaels.m@outlook.com>
2023-03-25 19:51:42 -0300

    deleted file

--- src/Components/SlideEmpreendimentos.jsx
+++ src/Components/SlideEmpreendimentos.jsx
@@ -7,8 +7,6 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
-import "./styles.css";
-
 // import required modules
 import { Pagination, Navigation } from "swiper";
 
