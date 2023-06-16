011b4bd5bc667a48913b403e8cff64452459eda5
Rafael Mauricio <rafaels.m@outlook.com>
2023-06-16 15:24:11 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	build: {
+		commonjsOptions: {
+			defaultIsModuleExports(id) {
+				const module = require(id);
+				if (module?.default) {
+					return false;
+				}
+				return "auto";
+			},
+		},
+	},
 	plugins: [react()],
 });
